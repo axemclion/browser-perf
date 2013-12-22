baf36f19ca072190296d5f9171fdc95d7cc39c15
Parashuram <code@nparashuram.com>
2013-12-22 13:35:09 -0800

    (test): Fixed test cases broken by change in options object

--- test/unit/options/browsers.spec.js
+++ test/unit/options/browsers.spec.js
@@ -6,21 +6,17 @@ var options = require('../../../lib/options');
 describe('Options', function() {
 	describe('Browsers', function() {
 		var test = options.defaults;
-		var conf = {
-			chrome: 0,
-			firefox: 1
-		}
 
 		it('default', function() {
 			var res = test({});
-			expect(res.browserConfig).deep.equal([options.browserConfig[conf.chrome]]);
+			expect(res.browserConfig).deep.equal([options.browserConfig.chrome]);
 		});
 
 		it('single browser as string', function() {
 			var res = test({
 				browsers: 'chrome'
 			});
-			expect(res.browserConfig).deep.equal([options.browserConfig[conf.chrome]]);
+			expect(res.browserConfig).deep.equal([options.browserConfig.chrome]);
 		});
 
 		it('multiple browsers as strings', function() {
@@ -28,8 +24,8 @@ describe('Options', function() {
 				browsers: 'chrome,firefox'
 			});
 			expect(res.browserConfig.length).to.equal(2);
-			expect(res.browserConfig[0]).deep.equal(options.browserConfig[conf.chrome]);
-			expect(res.browserConfig[1]).deep.equal(options.browserConfig[conf.firefox]);
+			expect(res.browserConfig[0]).deep.equal(options.browserConfig.chrome);
+			expect(res.browserConfig[1]).deep.equal(options.browserConfig.firefox);
 		});
 
 		it('string array', function() {
@@ -37,8 +33,8 @@ describe('Options', function() {
 				browsers: ['chrome', 'firefox']
 			});
 			expect(res.browserConfig.length).to.equal(2);
-			expect(res.browserConfig[0]).deep.equal(options.browserConfig[conf.chrome]);
-			expect(res.browserConfig[1]).deep.equal(options.browserConfig[conf.firefox]);
+			expect(res.browserConfig[0]).deep.equal(options.browserConfig.chrome);
+			expect(res.browserConfig[1]).deep.equal(options.browserConfig.firefox);
 		});
 
 		it('object array', function() {
@@ -50,8 +46,8 @@ describe('Options', function() {
 				}]
 			});
 			expect(res.browserConfig.length).to.equal(2);
-			expect(res.browserConfig[0]).deep.equal(options.browserConfig[conf.chrome]);
-			expect(res.browserConfig[1]).deep.equal(options.browserConfig[conf.firefox]);
+			expect(res.browserConfig[0]).deep.equal(options.browserConfig.chrome);
+			expect(res.browserConfig[1]).deep.equal(options.browserConfig.firefox);
 		});
 
 		it('respect debug flag', function() {
